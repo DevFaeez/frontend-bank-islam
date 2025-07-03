4bf683d94e7ca2db36072c85bb0264b41c6e08d0
faiszuddin <muhdfaez.gm@gmail.com>
2025-07-04 01:02:07 +0800

    update the index store

--- src/user/store/index.jsx
+++ src/user/store/index.jsx
@@ -2,26 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./slice/AuthSlice";
 import { dashboardReducer } from "./slice/DashboardSlice";
 import { billReducer } from "./slice/BillSlice";
-<<<<<<< HEAD
 import { transferReducer } from "./slice/TransferSlice";
 import { goalAccountReducer } from "./slice/GoalAccountSlice";
-=======
-import { useReducer } from "react";
 import { userReducer } from "./slice/UserProfileSlice";
->>>>>>> 6abea84280ce5d4735c96eaedfbcbfd60e646b25
 
 const store = configureStore({
-    reducer: {
+reducer: {
         auth: authReducer,
         dashboard: dashboardReducer,
         bill: billReducer,
-<<<<<<< HEAD
         transfer: transferReducer,
-        goalAccount: goalAccountReducer
-=======
+        goalAccount: goalAccountReducer,
         user: userReducer
->>>>>>> 6abea84280ce5d4735c96eaedfbcbfd60e646b25
-    }
+}
 })
 
 export {store}
